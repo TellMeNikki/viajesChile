370fab2e9913c3af6f601657fe6aa0507d8c208f
Tellmenikki <arayav.nicole@gmail.com>
2022-04-22 00:27:27 -0400

    5th - funcional model

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    //tooltip de nosotros
+  $(function () {
+    $('[data-bs-toggle="tooltip"]').tooltip()
+  });
   //smooth scroll
   $("a").on('click', function(event){
     if (this.hash !== "") {
@@ -11,7 +15,6 @@ $(document).ready(function(){
       });
     }
   });
-
   //agrega color de fondo al nav cuando se hace scroll y pasa los 550px
   $(window).scroll(function() {
     if ($(document).scrollTop() > 550) {
@@ -20,5 +23,22 @@ $(document).ready(function(){
       $("nav").removeClass("add-color");
     }
   });
-  
+  //conformación de mensaje enviado
+  $('.btn-enviar').on('click', function(){
+    alert ("Su mensaje ha sido enviado");
+  });
+  //toggle de iconos de nosotros
+  $('.nosotros .card1').on('click', function(){
+    $('.card1 .card-body').toggle();
+  });
+  $('.nosotros .card2').on('click', function(){
+    $('.card2 .card-body').toggle();
+  });
+  $('.nosotros .card3').on('click', function(){
+    $('.card3 .card-body').toggle();
+  });  
+  //toggle de destacados
+  $('.destacados .card').on('click', function(){
+    $('.destacados p').toggle();
+  });
 });
