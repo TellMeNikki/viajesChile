d74c18a707e63747cb38a5a308aa0c885d9a5275
Tellmenikki <arayav.nicole@gmail.com>
2022-04-21 21:27:00 -0400

    mockup ready -now add jquery functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
+  //smooth scroll
   $("a").on('click', function(event){
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 300, function(){
+      }, 100, function(){
         window.location.hash = hash;
       });
+    }
+  });
+
+  //agrega color de fondo al nav cuando se hace scroll y pasa los 550px
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 550) {
+      $("nav").addClass("add-color");
+    } else {
+      $("nav").removeClass("add-color");
     }
   });
   
